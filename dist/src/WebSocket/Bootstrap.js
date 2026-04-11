8b771f5fd834bd9705caec8c924e4070c7efc95b
Jackson Santos <jackson.santos@kukac.com.br>
2026-04-11 18:14:33 -0300

    removing dist

--- dist/src/WebSocket/Bootstrap.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.startWebSocketServer = startWebSocketServer;
-var WSService_1 = require("./WSService");
-function startWebSocketServer(server) {
-    new WSService_1.WSService(server);
-}
-//# sourceMappingURL=Bootstrap.js.map
